13fef38416b6abadbfffc474b40ba05205abdb48
ohs <ohs2607@gmail.com>
2024-01-03 15:13:22 +0900

    특수문자는 제자리에 두고 알파벳만 뒤집기

--- src/lecture/string/ReverseSpecificWord.java
+++ src/lecture/string/ReverseSpecificWord.java
@@ -3,10 +3,10 @@ package lecture.string;
 import java.util.Scanner;
 
 /**
- * 영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고,
- * 특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성
- *
- * a#b!GE*T@S -> S#T!EG*b@a
+ 영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고,
+ 특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성
+
+ a#b!GE*T@S -> S#T!EG*b@a
  */
 public class ReverseSpecificWord {
 
