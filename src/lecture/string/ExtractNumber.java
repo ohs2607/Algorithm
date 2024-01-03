c93c9a4ee8d59150dabcd2c60beff0148edd8e1f
ohs <ohs2607@gmail.com>
2024-01-03 15:09:26 +0900

    문자와 숫자가 섞인 문자열에서 숫자만 추출하기

--- src/lecture/string/ExtractNumber.java
+++ src/lecture/string/ExtractNumber.java
@@ -2,9 +2,8 @@ package lecture.string;
 
 import java.util.Scanner;
 
-/** 9. 문자와 숫자가 섞여있는 문자열이 주어지면, 그 중 숫자만 추출하고 그 순서대로 자연수를 만듦
- *  g0en2T0s8eSoft -> 208
- */
+// 9. 문자와 숫자가 섞여있는 문자열이 주어지면, 그 중 숫자만 추출하고 그 순서대로 자연수를 만듦
+// g0en2T0s8eSoft -> 208
 public class ExtractNumber {
 
     public static void main(String[] args) {
