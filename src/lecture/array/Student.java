8185700b3868b56a15dc4285133ff9f25a437244
ohs <ohs2607@gmail.com>
2024-01-03 15:28:05 +0900

    맨 앞에 서 있는 선생님이 볼 수 있는 학생의 인원수 구하기 (max값 두기)

--- src/lecture/array/Student.java
+++ src/lecture/array/Student.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
  * 학생을 일렬로 세웠을 때의 학생의 키가 앞에서부터 순서대로 주어지고
  * 맨 앞에 서 있는 선생님이 볼 수 있는 학생의 수를 구하기
 
- 8
- 130 135 148 140 145 150 150 153
+8
+130 135 148 140 145 150 150 153
  */
 public class Student {
 
@@ -25,14 +25,24 @@ public class Student {
     static int solution(int n, int[] students){
         int answer = 1; // 시작은 제일 첫번쨰 학생의 키가 기준
 
-        for (int i=0; i<n-1; i++){
+        /**for (int i=0; i<n-1; i++){
             if(students[i] < students[i+1]) {
                 answer++;
             } else {
                 // 앞에 있던 값이 더 크다면 자리 변경 후 비교할 수 있게
                 students[i+1] = students[i];
             }
+        }*/
+
+        // max 사용
+        int max = students[0]; // 제일 첫번쨰 학생의 키
+        for (int i=1; i<n; i++){
+            if(max < students[i]) {
+                answer++;
+                max = students[i];
+            }
         }
+
         return answer;
     }
 }
