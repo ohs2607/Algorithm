fe568683a822e49276519bc064257abf397b8522
ohs <ohs2607@gmail.com>
2024-01-03 15:07:38 +0900

    문자열에서 특정 문자의 개수

--- src/lecture/string/FindString.java
+++ src/lecture/string/FindString.java
@@ -3,6 +3,10 @@ package lecture.string;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ Computercooler
+ c
+ */
 public class FindString {
 
     public static void main(String[] args) throws IOException {
@@ -18,9 +22,7 @@ public class FindString {
         char c = Character.toLowerCase(ch.charAt(0));
 
         for (char word : str.toCharArray()) {
-            if(c == Character.toLowerCase(word)){
-                count++;
-            }
+            if(c == Character.toLowerCase(word)) count++;
         }
         return count;
     }
