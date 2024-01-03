5ffdb3a2ade5ca3daa211c31edf03144e57b9b9c
ohs <ohs2607@gmail.com>
2024-01-03 15:14:14 +0900

    문자열에서 특정 문자와 나머지 문자 사이 최소 거리 구히기

--- src/lecture/string/StrDistance.java
+++ src/lecture/string/StrDistance.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /* 한 개의 문자열 s와 문자 t가 주어지면
    문자열 s의 각 문자가
    문자 t와 떨어진 최소거리를 출력하는 프로그램 */
-/** teachermode e -> 1 0 1 2 1 0 1 2 2 1 0 */
+// teachermode e -> 1 0 1 2 1 0 1 2 2 1 0
 public class StrDistance {
 
     public static void main(String[] args) {
