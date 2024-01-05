7cbf8bf04e19f632279a7329d3c8e082ddb243a8
ohs <ohs2607@gmail.com>
2024-01-05 11:29:48 +0900

    덧칠하기 (진행중)

--- src/programmers/skill_check/Paint.java
+++ src/programmers/skill_check/Paint.java
@@ -1,5 +1,8 @@
 package programmers.skill_check;
 
+// 벽 전체 길이 -> m
+// 롤러의 길이  -> n
+// section    -> 칠해야할 구역
 public class Paint {
 
     public static void main(String[] args) {
@@ -12,23 +15,10 @@ public class Paint {
 
     public int solution(int n, int m, int[] section){
         int answer = 0;
-        int finishArea = 0;
+        // 전체 n에서 section에 있는 구역이 나오면 비교
+        // 벽길이는 1 ~ n
+        for(int i = 1; i<=n; i++){
 
-        for(int i = 0; i<section.length; i++){
-            // 2부터면 전체 8에서 -> 2~5까지 (2+m-1)
-            // section 첫번쨰요소의 다음 값들이 (2+m-1)보다 작으면 answer 한번 추가한채로 넘어가고
-            // 크다면 answer 에 한번 더 ++
-            //section[i] + m -1 // 여기까지가 첫번쨰로 칠해지는 영역
-
-            if(i == 0){
-                finishArea = section[i] + m -1;
-                answer++;
-            }
-
-            if(section[i] > finishArea){
-                // 두번쨰 구역부터 처음 칠해지는 영역보다 작으면 넘어가고 크면 answer 에 하나 더함
-                answer++;
-            }
         }
 
         return answer;
