b4d66e4c635efc410306c8129a6436dc56f086c5
ohs <ohs2607@gmail.com>
2024-01-07 19:56:32 +0900

    정수를 뒤집은 후 소수면 return (숫자 그대로 뒤집기)

--- src/lecture/array/ReversePrime.java
+++ src/lecture/array/ReversePrime.java
@@ -8,17 +8,20 @@ import java.util.Scanner;
    그 뒤집은 수가 소수이면 그 소수를 출력하는 프로그램 */
 
 // res = res * 10 + t
-/** 예를 들어 32를 뒤집으면 23이고, 23은 소수, 단 910을 뒤집으면 19 - 첫자리부터 연속된 0 무시
-9
-32 55 62 20 250 370 200 30 100
-*/
+
+/**
+ * 예를 들어 32를 뒤집으면 23이고, 23은 소수, 단 910을 뒤집으면 19 - 첫자리부터 연속된 0 무시
+ * 9
+ * 32 55 62 20 250 370 200 30 100
+ */
 public class ReversePrime {
 
     public static void main(String[] args) {
+        ReversePrime rp = new ReversePrime();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for (int i=0; i<n; i++) arr[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
         for (int rstl : solution(n, arr)) {
             System.out.print(rstl + " ");
@@ -26,23 +29,23 @@ public class ReversePrime {
     }
 
     // 소수인지 확인하는 함수
-    static boolean isPrime(int num){
-        if (num == 1) {return false;
-        for (int i=2; i<num; i++){
-            if (num % i ==0) return false;
+    public boolean isPrime(int num) {
+        if (num == 1) return false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) return false;
         }
         return true;
     }
 
-    static List<Integer> solution(int n, int[] arr){
+    public static List<Integer> solution(int n, int[] arr) {
         List<Integer> result = new ArrayList<>();
 
-        for (int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             int tmp = arr[i];
             // res에 뒤집힌 결과가 저장
             int res = 0;
 
-            while (tmp > 0){
+            while (tmp > 0) {
                 // 1. tmp를 10으로 나눠서 나머지를 구함 (나머지는 마지막 자리가 됨 - 123이라면 3)
                 int t = tmp % 10;
                 // 2. 자릿수를 뒤집는 공식을 적용한다
