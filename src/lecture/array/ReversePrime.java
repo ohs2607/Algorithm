03b94982cff94d84e79deeee0db5f9ebf620a075
ohs <ohs2607@gmail.com>
2024-01-07 19:57:29 +0900

    정수를 뒤집은 후 소수면 return (숫자 그대로 뒤집기)

--- src/lecture/array/ReversePrime.java
+++ src/lecture/array/ReversePrime.java
@@ -29,7 +29,7 @@ public class ReversePrime {
     }
 
     // 소수인지 확인하는 함수
-    public boolean isPrime(int num) {
+    public static boolean isPrime(int num) {
         if (num == 1) return false;
         for (int i = 2; i < num; i++) {
             if (num % i == 0) return false;
