5e6b06931ab45c0c3fb82ad5e8f96d10ca8b169c
ohs <ohs2607@gmail.com>
2024-01-08 15:14:52 +0900

    1181번 - 단어 정렬 (Arrays.sort 재정의)

--- src/baekjoon/silver/SortWord.java
+++ src/baekjoon/silver/SortWord.java
@@ -13,63 +13,60 @@ public class SortWord {
         for (int i = 0; i < num; i++) {
             wArr[i] = sc.nextLine();
         }
-        System.out.println(solution(num, wArr));
+        for (String str : solution(num, wArr)) {
+            System.out.println(str);
+        }
     }
 
     static List<String> solution(int cnt, String[] wArr) {
-        List<String> result = new ArrayList<>();
-        boolean continuous = true;
+        /* sort를 재정의한다 */
+        /**
+         * 파라미터가 1개인 sort는 Comparator를 default로 정렬해왔던것. 정수의 경우에는 문제 없지만
+         * 문자열의 경우에는 Comparator 재정의가 필요하다.
+         * 따라서 2번쨰 파라미터로 Comparator 객체를 생성해서 넘겨준다
+         */
 
-        // 1. 중복 제거
-        for (int i = 0; i < cnt; i++) {
-            if (!result.contains(wArr[i])) result.add(wArr[i]);
-        }
-
-        // 2. 길이가 짧은 단어 순서대로 정렬
-        while (continuous) {
-            continuous = false;
-
-            for (int i = 0; i < result.size() - 1; i++) {
-
-                if (result.get(i).length() > result.get(i + 1).length()) {
-                    String tmp = result.get(i);
-                    result.set(i, result.get(i + 1));
-                    result.set(i + 1, tmp);
-
-                    continuous = true;
+        /* 1. 순서대로 정렬 */
+        Arrays.sort(wArr, new Comparator<String>() {
+            /* Comparator의 메서드 중 compare 오버라이딩 - 재정의 */
+            @Override
+            public int compare(String s1, String s2) {
+                // 반환 값이 int인데, 왜냐면 비교 후 결과를 음수, 양수로 판단해서 정렬하기 떄문이다
+                // 단순 길이 비교로 정렬과 & 길이가 같을 경우 사전순서로 정렬도 있기에 조건을 준다
+                if (s1.length() == s2.length()){
+                    // 길이가 같다면 사전순서로 정렬해야 한다.
+                    return s1.compareTo(s2);
+                } else {
+                    // 양수가 나온다면 s1이 더 길기 때문에 s1과 s2의 자리가 바뀌게 된다
+                    return s1.length() - s2.length();
                 }
             }
+        });
+
+        /* 2. 중복 제거  */
+        List<String> result = new ArrayList<>();
+        for (String str : wArr) {
+            if (!result.contains(str)) result.add(str);
         }
 
-
-        for (int i = 0; i < result.size() - 1; i++) {
-
-            if (result.get(i).length() == result.get(i + 1).length()) {
-                List<String> temp = Arrays.asList(result.get(i), result.get(i + 1));
-                temp.sort(null);
-
-                result.set(i, temp.get(0));
-                result.set(i + 1, temp.get(1));
-            }
-        }
         return result;
     }
 
 
 }
 /**
- * 13
- * but
- * i
- * wont
- * hesitate
- * no
- * more
- * no
- * more
- * it
- * cannot
- * wait
- * im
- * yours
+13
+but
+i
+wont
+hesitate
+no
+more
+no
+more
+it
+cannot
+wait
+im
+yours
  */
