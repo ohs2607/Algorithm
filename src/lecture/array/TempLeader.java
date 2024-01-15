5474ac3d2b3c7c07ce3ceb416309d1947668bbf8
ohs <ohs2607@gmail.com>
2024-01-15 17:57:19 +0900

    이해하는 중

--- src/lecture/array/TempLeader.java
+++ src/lecture/array/TempLeader.java
@@ -1,63 +1,62 @@
 package lecture.array;
 
-import java.util.*;
+
+import java.util.Scanner;
 
 /**
-    그는 자기반 학생 중에서 1학년부터 5학년까지 지내오면서
-    한번이라도 같은 반이었던 사람이 가장 많은 학생을 임시 반장으로 정하려 한다.
-    첫 줄에 임시 반장으로 정해진 학생의 번호를 출력한다.
+ * 그는 자기반 학생 중에서 1학년부터 5학년까지 지내오면서
+ * 한번이라도 같은 반이었던 사람이 가장 많은 학생을 임시 반장으로 정하려 한다.
+ * 첫 줄에 임시 반장으로 정해진 학생의 번호를 출력한다.
  */
 public class TempLeader {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] arr = new int[n][n];
+        int[][] arr = new int[n + 1][6];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
-
         System.out.println(resolve(n, arr));
     }
 
-    static int resolve(int n, int[][] arr){
-        // 같은 열에서 행을 비교해야 하니 i가 변경이 있어야함. j는 i가 다 돌때까지 고정
+    static int resolve(int n, int[][] arr) {
+        // arr[i][k] == arr[j][k]
+        // i와 j는 학생 번호이며, k는 학년을 의미
+        int answer = 0, max = 0;
 
-        int[] temp = new int[n];
-        int[] result = new int[n]; // 각 학생의 같은반 횟수 결과
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
 
-        for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
-                // 0,0 1,0 2,0 3,0 4,0  /  1,1 2,1 3,1 4,1 ...
-                // arr[j][i] 이 해당 열에서 몇개가 있는지 확인하면 될텐데
-                // 열을 따로 빼는 방법이 있을텐데 일단 모르니 노가다
-                // 한열을 다 넣고나서 열의 요소 비교
-                for (int k=0; k<n; k++){
-                    temp[k] = arr[k][i];
-                }
-                //System.out.println("temp = " + Arrays.toString(temp));
-                //System.out.println("i: " + i + ", j: " + j + " // " + temp[i] + " " +  arr[j][i]);
-                for (int k=0; k<n; k++){
-                    if (temp[k] == arr[j][i]){
-                        result[k]++;
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        // i=1, j=1일떄 계속 같은거 아니야..?
+                        /*
+                        1,1 1,1 / 1,2 1,2 / 1,3 1,3
+                        1,1 2,1 / 1,2 2,2 / 1,3 2,3
+                        1,1 3,1 / 1,2 3,2 / 1,3 3,3
+                        한명의 학생이 기준이 되어서 각 학년별로 다른 학생들과 비교
+
+                        */
+
+                        cnt++;
+                        break;
                     }
                 }
-
             }
-            //System.out.println();
+            System.out.println("max = " + max);
+            System.out.println("cnt = " + cnt);
+            System.out.println("-----------------------------");
+            if (cnt > max) {
+                max = cnt;
+                answer = i;
+            }
         }
-        //System.out.println("결과: " + Arrays.toString(result));
-        Map<Integer, Integer> rank = new HashMap<>();
-        for (int i=0; i<n; i++){
-            rank.put(result[i], i+1);
-        }
-        List<Integer> list = new ArrayList<>(rank.keySet());
-        list.sort(Comparator.reverseOrder());
-
-        return rank.get(list.get(0));
+        return answer;
     }
 
 }
@@ -68,4 +67,6 @@ public class TempLeader {
 5 5 2 4 4
 6 5 2 6 7
 8 4 2 2 2
+ * 학생 수를 세고 있기 때문에 3번 학생과 4번 학생이 2,3학년때 모두 같은 반이었어도
+ * 한번만 카운팅이 되어야 한다
  */
