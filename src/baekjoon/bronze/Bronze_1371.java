d088501d71842aec8c76bd6a2b7259c9b3b2b928
ohs <ohs2607@gmail.com>
2024-01-19 11:28:25 +0900

    여러줄의 문장들 중에서 가장 많은 글자 찾기 (진행중)

--- src/baekjoon/bronze/Bronze_1371.java
+++ src/baekjoon/bronze/Bronze_1371.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.IntStream;
 
 /*
 영어에서는 어떤 글자가 다른 글자보다 많이 쓰인다. 예를 들어, 긴 글에서 약 12.31% 글자는 e이다.
@@ -22,16 +23,33 @@ public class Bronze_1371 {
         while ((str = br.readLine()) != null && !str.isEmpty()) {
 
             for (int i = 0; i < str.length(); i++) {
+                // A : 65, a : 97, 공백 : 32
                 if (str.charAt(i) != ' ') {
                     alphabet[str.charAt(i) - 'a']++;
-                }
 
-                if (alphabet[str.charAt(i) - 'a'] > max) {
-                    max = alphabet[str.charAt(i) - 'a'];
+                    if (alphabet[str.charAt(i) - 'a'] > max) {
+                        max = alphabet[str.charAt(i) - 'a'];
+                    }
                 }
             }
-
         }
+        // max값은 이미 알지만 IntStream으로 최댓값을 찾는 방법
+        int maxNum = IntStream.of(alphabet).max().orElse(0);
+
+        // 배열의 요소들 중 최댓값과 같은 요소들의 자릿수만 찾는다
+        /* 위에 max를 구했지만 다시 최댓값을 구해서 filter에 사용한 이유는
+            java: local variables referenced from a lambda expression must be final or effectively final
+            람다 표현식에서는 사용 가능한 지역변수라도 이미 값이 한번이상 변경된 지역변수는 못쓴다
+
+            따라서 while문에서 값이 변경되었기 때문에 새로 구한 maxNum을 사용해야 한다
+        */
+        IntStream.range(0, alphabet.length)
+                .filter(i -> alphabet[i] == maxNum)
+                        .forEach(i -> {
+                            // i는 index번호이자, 알파벳과 연결된 숫자. 따라서 알파벳을 알려면 문자로 변환하면 된다.
+                            char c = (char) (i + 'a');
+                            System.out.println(c);
+                        });
 
         // 0~25까지 자리는 a~z자리를 의미. 각 자리에는 해당 문자가 포함된 개수가 들어있음
         System.out.println(Arrays.toString(alphabet));
